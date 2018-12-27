d47c6751730971be0d8d4a3af1927a412db44644
Dan Gale <danjgale@gmail.com>
2018-12-27 11:49:07 -0700

    included nipype as dependency

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnstools',
+    version='0.0.1',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"]),
+    license='MIT',
+    author='Dan Gale',
+    long_description=open('README.md').read(),
+    url='https://github.com/danjgale/cns-imaging-tools/',
+    install_requires=[
+        'numpy>=1.15',
+        'scipy>=1.1',
+        'nipype>=1.1.1'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+    setup_requires=['pytest-runner']
+)
