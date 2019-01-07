999abe00ef23e29281348317d20c5ea587c4fb88
Dan Gale <danjgale@gmail.com>
2019-01-07 11:17:54 -0500

    cleaned up _make_event_files, allowed add_subject to handle multiple sessions properly

--- cnstools/bids.py
+++ cnstools/bids.py
@@ -14,32 +14,36 @@ def _num_format(x):
     return '0{}'.format(x) if x < 10 else str(x)
 
 
-def _make_event_files(df, pid):
-    df = df.copy()
-    run = df['run'].iloc[0]
-    task = df['task'].iloc[0]
+def _make_event_files(data, pid, out_path):
+    df = data.copy()
+    run, task = (df['run'].iloc[0], df['task'].iloc[0])
     pid = _num_format(pid)
-    (df.drop('run', axis=1, inplace=True)
-       .to_csv('sub-{pid}_task-{task}_run-{run}_events.tsv', sep='\t')
-    )
+    fname = 'sub-{pid}_task-{task}_run-{run}_events.tsv'
+    df.drop('run', axis=1, inplace=True).to_csv(os.path.join(out_path, fname),
+                                                sep='\t')
 
 
 class BidsData(object):
     def __init__(self, data_path):
         self.data_path = data_path
-        self.pid_pairs = []
+        self.sub_id_pairs = []
+        self.__sub_id_list = []
 
 
-    def add_subject(self, sub_id, dicom_path, config, session_number=None):
+    def add_subject(self, sub_id, dicom_path, config, session=None):
 
-        bids = Dcm2bids(dicom_path, sub_id, config)
+        bids = Dcm2bids(dicom_path, sub_id, config, session=session)
         bids.run()
 
-        sub_id_pair = {
-            'participant_original': os.path.basename(dicom_path),
-            'participant_id': sub_id
-        }
-        self.sub_id_pairs.append(sub_id_pair)
+        self.__sub_id_list.append(sub_id)
+        if sub_id not in self.__sub_id_list:
+            # only add one pair for a subject; prevent duplicates arising
+            # from multiple sessions
+            sub_id_pair = {
+                'participant_original': os.path.basename(dicom_path),
+                'participant_id': sub_id
+            }
+            self.sub_id_pairs.append(sub_id_pair)
 
 
     def add_event_files(self, fname):
@@ -63,9 +67,5 @@ class BidsData(object):
             df.to_csv(os.path.join(self.data_path, 'participants.tsv'), sep='\t')
         except KeyError:
             print("Participant data incomplete or incorrect. Must include "
-                  "columns 'participant_id', 'sex', and 'age'. Leaving "
-                  "participants.tsv blank.")
+                  "columns 'participant_id', 'sex', and 'age'")
 
-
-    def set_task(self):
-        pass
